5552925eb9a2dafd7d43b092aef1e329bb680bf4
slimanfl <slimanite1996@gmail.com>
2023-12-27 17:11:30 -0800

    init project and install nextui with providers and next theme

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+}
