d6f6b85a199f67d676ecfd246aefc603c83bd434
slimanfl <slimanite1996@gmail.com>
2023-12-27 17:31:21 -0800

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      hello world !!! 
     </main>
-  )
+  );
 }
